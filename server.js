01966891f29475f2c25a10751f455c1be7057b6c
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2017-10-25 16:16:25 -0400

    add sheet api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const app = require('./src/app')
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT || 3001, function () {
+    console.log('Example app listening on port 3001!')
   })
--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 const Express = require('express')
+const cors = require('cors')
 const app = Express()
 
 app.get('/', function (req, res) {
   res.send('Hello Expense Break!')
+})
+
+app.get('/sheets', cors(), function (req, res) {
+  res.json([
+    {
+      name: 'Personal Expenses',
+      categary: 'daily'
+    },
+    {
+      name: 'Goa Trip Expenses',
+      categary: 'one-time'
+    },
+    {
+      name: 'Roomate Expense',
+      categary: 'monthly'
+    }
+  ])
 })
 
 module.exports = app
