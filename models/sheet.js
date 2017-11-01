855bd88e7f11af2a2ab9dfeeb96839219da69094
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2017-10-31 22:49:14 -0400

    Add Coral Routes

--- models/sheet.js
+++ models/sheet.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
+mongoose.Promise = global.Promise
+
 const sheetSchema = new mongoose.Schema({
-    name: String,
-    category: String
+  name: String,
+  category: String
 })
 
-module.exports = mongoose.model('Sheet', sheetSchema)
+exports = module.exports = mongoose.model('Sheet', sheetSchema)
